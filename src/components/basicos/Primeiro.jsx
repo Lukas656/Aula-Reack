6ba0523bb63abe731fabed46910c4d7109d27e3f
Lucas <Lukas656>
2023-08-04 18:11:23 -0300

    Iniciando propriedades e componentes

---
+++ src/components/basicos/Primeiro.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Primeiro() {
+   
+    const msg = 'Seja bem Vindo(a)!!'
+   
+    return (
+        <>
+            <h2>Primeiro Componente</h2>
+            <p>{msg}</p>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import ReactDOM from "react-dom"
 import './index.css'
+import Primeiro from "./components/basicos/Primeiro"
+import ComParametro from "./components/basicos/Comparametro"
 
 ReactDOM.render(
-    'Olá', 
+    <>
+    <Primeiro/>
+    <ComParametro 
+        titulo="Segundo Componente Aula React"
+        aluno="Lucas Santos"
+        nota={10}
+    
+    />
+    </>, 
     document.getElementById('root')
 )
