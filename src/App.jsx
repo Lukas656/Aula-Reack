f0509c7364db643c5ba7052e7f77f76e8622b587
Lucas <Lukas656>
2023-08-04 19:21:08 -0300

    Criando Cards e Organizando Exercicios em blocos

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
 import React from "react";
-
+import './App.css'
 import Primeiro from "./components/basicos/Primeiro"
 import ComParametro from "./components/basicos/Comparametro"
 import Aleatorio from "./components/basicos/Aleatorio";
-
+import Card from "./components/layout/Card";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function (props){
     return (
         <>
         <h1>Fundamentos REACT</h1>
-            <Primeiro />
+        <div className="cards">
+        <Card titulo="#04 - Exemplo Card" color="rgb(222, 5, 84)">
+
+        </Card>
+
+        <Card titulo="#03 - Aleatório" color="rgb(22, 5, 84)">
+            <Aleatorio max={1} min={60}/>
+        </Card>
+
+        <Card titulo="#02 - Com Parametro" color="rgb(222, 5, 8)">
             <ComParametro
                 titulo="Segundo Componente Aula React"
                 aluno="Lucas Santos"
                 nota={10}
 
             />
-            <Aleatorio max={1} min={60}/>
+        </Card>
+        <Card titulo="#01 - Primeiro" color="rgb(22, 50, 100)">
+            <Primeiro />
+        </Card>
+        </div>
         </>
     )
 }
